692954e15766f864f005bd621995e7e19d1dd297
Esther Wandera <wanderaesther809@gmail.com>
2025-10-23 02:08:11 +0300

    initial commit

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { image } from "../data/data";
+import image from "../images/profile.jpg"; // or whatever image file the starter gives you
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>I’m a passionate developer who loves building web apps!</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
